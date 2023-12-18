7e64161e14df62f412baca24356376c05bdb6fa9
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-12-18 17:25:31 +0100

    initial commit

---
+++ schemas/index.ts
@@ -0,0 +1,10 @@
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import testemonials from "./testemonials"
+import workExperience from "./workExperience"
+import works from "./works"
+
+export const schemaTypes = [testemonials,abouts,brands,contact,experiences,skills,workExperience,works]
